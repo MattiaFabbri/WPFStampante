038dc375ab856bc9482e98b932bc499844879324
Mattia Fabbri <145842000+MattiaFabbri@users.noreply.github.com>
2023-12-19 14:46:45 +0100

    .

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPFStampante.Models;
+
+namespace WPFStampante
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        Stampante stampante = new Stampante();
+
+        private void Stampa_Click(object sender, RoutedEventArgs e)
+        {
+
+            Pagina pagina = new Pagina(Convert.ToInt32(ValCiano.Text), Convert.ToInt32(ValGiallo.Text), Convert.ToInt32(ValGiallo.Text), Convert.ToInt32(ValNero.Text));
+            stampante.Stampa(pagina);
+            ciano.Text = Convert.ToString(stampante.StatoInchiostro(0));
+            magenta.Text = Convert.ToString(stampante.StatoInchiostro(2));
+            giallo.Text = Convert.ToString(stampante.StatoInchiostro(1));
+            nero.Text = Convert.ToString(stampante.StatoInchiostro(3));
+            Fogli.Text = Convert.ToString(stampante.StatoCarta());
+        }
+        private void StampaRandom_Click(object sender, RoutedEventArgs e)
+        {
+
+            Pagina pagina = new Pagina();
+            stampante.Stampa(pagina);
+            ciano.Text = Convert.ToString(stampante.StatoInchiostro(0));
+            magenta.Text = Convert.ToString(stampante.StatoInchiostro(2));
+            giallo.Text = Convert.ToString(stampante.StatoInchiostro(1));
+            nero.Text = Convert.ToString(stampante.StatoInchiostro(3));
+            Fogli.Text = Convert.ToString(stampante.StatoCarta());
+        }
+
+
+        private void ControlloValore(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            // Verifica se il riferimento a TextBox è nullo
+            if (textBox != null)
+            {
+                // Controlla se il testo inserito è tra i caratteri consentiti
+                if ((e.Text != "0" && e.Text != "1" && e.Text != "2" && e.Text != "3") || textBox.Text.Length >= 1)
+                {
+                    e.Handled = true; // Impedisci l'inserimento del carattere
+                }
+            }
+        }
+
+        private void riempiCiano_Click(object sender, RoutedEventArgs e)
+        {
+            stampante.SostituisciColore(0);
+            ciano.Text = Convert.ToString(stampante.StatoInchiostro(0));
+        }
+
+        private void riempiGiallo_Click(object sender, RoutedEventArgs e)
+        {
+            stampante.SostituisciColore(1);
+            giallo.Text = Convert.ToString(stampante.StatoInchiostro(1));
+        }
+
+        private void riempiMagenta_Click(object sender, RoutedEventArgs e)
+        {
+            stampante.SostituisciColore(2);
+            magenta.Text = Convert.ToString(stampante.StatoInchiostro(2));
+        }
+
+        private void riempiNero_Click(object sender, RoutedEventArgs e)
+        {
+            stampante.SostituisciColore(3);
+            nero.Text = Convert.ToString(stampante.StatoInchiostro(3));
+        }
+
+        private void RicaricaFogli_Click(object sender, RoutedEventArgs e)
+        {
+            stampante.AggiungiCarta(Convert.ToInt32(ValFogli.Text));
+            Fogli.Text = Convert.ToString(stampante.StatoCarta());
+
+        }
+    }
+}
---
+++ Models/Stampante.cs
@@ -0,0 +1,60 @@
+﻿namespace WPFStampante.Models
+{
+    /*    
+   - 4 serbatoi di colore: CMYB e un cassetto di fogli (tutte property int)
+   - un metodo int StatoInchiostro( Colore c ) che torna la quantità di inchiostro presente nei 4 serbatoi.
+   - un metodo int StatoCarta() che mi ritorna la quantità di fogli nel cassetto
+   - un metodo void SostituisciColore( Colore c ) che rimpiazza un serbatoio di inchiostro e lo riporta a 100
+   - un metodo void AggiungiCarta( int qta ) che aggiunge fogli fino ad un max di 200
+   */
+    public class Stampante
+    {
+
+        public int Fogli;
+        //ciano = 0,yellow = 1, magenta = 2, nero = 3
+        public int[] Inchiostro { get; set; }
+        public Stampante()
+        {
+            Inchiostro = new int[4] { 100, 100, 100, 100 };
+            Fogli = 200;
+        }
+        //- un metodo bool Stampa( Pagina p ) (che torna false, se l'inchiostro non è sufficiente per stampare)
+        public bool Stampa(Pagina p)
+        {
+            if (Fogli > 0 && (Inchiostro[0] >= p.C && Inchiostro[1] >= p.Y && Inchiostro[2] >= p.M && Inchiostro[3] >= p.B))
+            {
+                Inchiostro[0] -= p.C;
+                Inchiostro[1] -= p.Y;
+                Inchiostro[2] -= p.M;
+                Inchiostro[3] -= p.B;
+                Fogli--;
+                return true;
+            }
+            return false;
+        }
+        //un metodo int StatoInchiostro( Colore c ) che torna la quantità di inchiostro presente nei 4 serbatoi.
+        public int StatoInchiostro(int c)
+        {
+            return Inchiostro[c];
+        }
+        //un metodo int StatoCarta() che mi ritorna la quantità di fogli nel cassetto
+        public int StatoCarta()
+        {
+            return Fogli;
+        }
+        //un metodo void SostituisciColore( Colore c ) che rimpiazza un serbatoio di inchiostro e lo riporta a 100
+        public void SostituisciColore(int c)
+        {
+            Inchiostro[c] = 100;
+        }
+        //un metodo void AggiungiCarta( int qta ) che aggiunge fogli fino ad un max di 200
+        public void AggiungiCarta(int qta)
+        {
+            if (qta > 0 && (qta + Fogli) <= 200)
+            {
+                Fogli += qta;
+            }
+
+        }
+    }
+}
